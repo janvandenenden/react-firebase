59230581d14a4abed2ae839de0d91e692f7862b6
Jan Van den Enden <jan@enden.be>
2019-04-01 14:41:30 +0200

    added id from props in ProjectDetails

--- react-firebase/src/components/projects/ProjectDetails.js
+++ react-firebase/src/components/projects/ProjectDetails.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-const ProjectDetails = () => {
+const ProjectDetails = (props) => {
+const id = props.match.params.id;
   return (
     <div className="container section project-details">
         <div className="card z-depth-0">
             <div className="card-content">
-                <span className="card-title">Project Title</span>
+                <span className="card-title">Project Title - {id}</span>
                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint ipsa, eaque ratione ipsam praesentium ullam dolore, dicta, nisi consequatur nulla reprehenderit. Rerum, adipisci sit. Sit magnam rerum nostrum quae recusandae.</p>
             </div>
             <div className="card-action grey lighten-4 grey-text">
