024bfe5797c3c8f0dbf11ac4675f1cca19f67234
Jan Van den Enden <jan@enden.be>
2019-04-01 14:14:15 +0200

    added dashboard and routes

--- react-firebase/src/App.js
+++ react-firebase/src/App.js
@@ -3,16 +3,21 @@ import React, { Component } from 'react';
 // Link is your replacement for anchor tags.
 // Route is the conditionally shown component based on matching a path to a URL.
 // Switch returns only the first matching route rather than all matching routes.
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 // When we want to use a component that we created we need to import it.
 import Navbar from './components/layout/Navbar.js';
+import Dashboard from './components/dashboard/Dashboard.js';
+// We use a route for the Dashboard as it shouldn't be showed on all pages. The Switch-component ensures that the component is only shown on the first matching route.
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-      <div className="App">
+      <div className="App">       
         <Navbar />
+        <Switch>
+          <Route path='/' component={Dashboard} />
+        </Switch>
       </div>
       </BrowserRouter>
     );
