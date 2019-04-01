84b82e1ca9d7042c8adb980f269afd19541b0023
Jan Van den Enden <jan@enden.be>
2019-04-01 15:10:31 +0200

    add signIn and signUp forms

--- react-firebase/src/App.js
+++ react-firebase/src/App.js
@@ -11,6 +11,8 @@ import Dashboard from './components/dashboard/Dashboard.js';
 // The Switch-component ensures that the component is only shown on the first matching route.
 // The Route exact path means it has to be an exact match
 import ProjectDetails from './components/projects/ProjectDetails'
+import SignIn from './components/auth/SignIn'
+import SignUp from './components/auth/SignUp'
 
 class App extends Component {
   render() {
@@ -21,6 +23,8 @@ class App extends Component {
         <Switch>
           <Route exact path='/' component={Dashboard} />
           <Route path='/project/:id' component={ProjectDetails} />
+          <Route path='/signin' component={SignIn} />
+          <Route path='/signup' component={SignUp} />
         </Switch>
       </div>
       </BrowserRouter>
